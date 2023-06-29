4b3944de169107698c4de51df6c7b43d34d7d55e
Daniil Trapeznikov <danntrp@gmail.com>
2023-06-29 14:47:43 +0300

    Initial commit

---
+++ app/src/main/java/com/danntrp/fixtures/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danntrp.fixtures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
