9c1570d07daee02cc81816fce4f53d2315c7f4ad
Daniil Trapeznikov <danntrp@gmail.com>
2023-07-01 17:24:59 +0300

    Added splash screen with 2 second delay

--- app/src/main/java/com/danntrp/fixtures/MainActivity.kt
+++ app/src/main/java/com/danntrp/fixtures/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.danntrp.fixtures
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -14,8 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -26,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavView.apply {
             setupWithNavController(navController)
+            setOnItemReselectedListener {  }
         }
     }
 
